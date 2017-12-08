b45d32b7dae43f5cf2dfec8564982645ec2ed53d
rojo5 <rojo5@pt-azul>
2017-12-08 18:33:09 +0100

    Acabada la puntuación quedan el desbloqueo de niveles

--- trivial.php
+++ trivial.php
@@ -8,9 +8,6 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <?php
-//        include './obtenerDatos.php';
-        ?>
 
         <!--JAVASCRIPT-->
         <script src="js/preguntas.json" type="text/javascript"></script>
@@ -105,18 +102,32 @@ and open the template in the editor.
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="titulo text-center">
+                        <h3 class="grosor">
+                            <span id="1"class="text-">1</span>
+                            <span id="2">2</span>
+                            <span id="3">3</span>
+                            <span id="4">4</span>
+                            <span id="5">5</span>
+                            <span id="6">6</span>
+                            <span id="7">7</span>
+                            <span id="8">8</span>
+                            <span id="9">9</span>
+                            <span id="10">10</span>
+
+                        </h3>
+                    </div>
+                </div>
                 <div class="col"><br></div>
                 <div class="row">
                     <div class="col-xs-1"></div>
-                    <div class="col-xs-10" id="enunciado">
-
-                    </div>
+                    <div class="col-xs-10" id="enunciado"></div>
                     <div class="col-xs-1"></div>
                 </div>
                 <div class="row"> 
                     <div class="col-xs-1"></div>
-                    <div class="col-xs-10" id="respuestas">
-                    </div>
+                    <div class="col-xs-10" id="respuestas"></div>
                     <div class="col-xs-1"></div>
                 </div>
             </div>
@@ -137,12 +148,6 @@ and open the template in the editor.
             }
         }
 
-//        cambiaBotones();
-//
-//        function cambiaBotones() {
-//        $("#respuestas").append('<button class=" btn btn-block btn-primary btn-lg respuestas">' + $. + '</button>');
-//            
-//        }
     </script>
     <!--CAMBIAR DE MATERIAS A NIVELES-->
     <script>
@@ -168,6 +173,7 @@ and open the template in the editor.
                 $('#preguntas').fadeIn('slow');
             });
         });
+
     </script>
 
     <!--LEER FICHERO JSON-->
@@ -175,13 +181,16 @@ and open the template in the editor.
         var posicion;
         var longitud;
         var correcta;
+        var contador = 1;
+        var aciertos = 0;
+
         function crearJSON() {
             $.getJSON("js/preguntas.json", function (cuestiones) {
                 longitud = Object.keys(cuestiones["pregunta"]).length;
                 posicion = Math.floor(Math.random() * longitud);
                 correcta = cuestiones["pregunta"][posicion]["correcta"];
-                console.log(correcta);
-
+                $("#" + contador).addClass("text-primary");
+                
                 $("#enunciado").append('<button class=" btn btn-default btn-lg btn-block">' + cuestiones["pregunta"][posicion]["enunciado"] + '</button>');
                 $("#respuestas").append('<button id="btn-1" class=" btn btn-block btn-primary btn-lg respuestas" value="1" onclick="comprueba(correcta,1, this)">' + cuestiones["pregunta"][posicion]["r1"] + '</button>');
                 $("#respuestas").append('<button id="btn-2" class=" btn btn-block btn-primary btn-lg respuestas" value="2" onclick="comprueba(correcta,2, this)">' + cuestiones["pregunta"][posicion]["r2"] + '</button>');
@@ -190,9 +199,7 @@ and open the template in the editor.
             }
             );
         }
-    </script>
 
-    <script>
         function filtrarPorAsignatura(materia) {
             console.log(materia);
             var parametro = {
@@ -204,48 +211,90 @@ and open the template in the editor.
                 url: "obtenerDatos.php",
                 type: 'POST',
                 success: function (response) {
+                    $("#enunciado").text('');
+                    $("#respuestas").text('');
                     crearJSON();
-                    console.log("he pasado");
+                    
                 }
             });
         }
-    </script>
 
-    <script>
         function comprueba(_correcta, seleccionada, comp) {
             var correcta = _correcta;
             var eleccion = seleccionada;
             var id = comp.id;
-            console.log("El boton es:" + id);
-            console.log(correcta);
-            console.log(eleccion);
+            
+            console.log("Respuesta "+correcta);
+            $(".respuestas").on("click", function () {
+                $(".respuestas").prop('disabled', true);
+            });
+
+
 
             if (correcta == eleccion) {
-                $("#"+id).text("CORRECTO")
+                aciertos++;
+                $("#" + id).text("CORRECTO")
                         .addClass("btn btn-block btn-success btn-lg")
                         .fadeOut("slow")
                         .fadeIn("slow", function () {
-                            sigue(id);
+                            $("#" + contador).addClass("text-success");
+                            sigue();
+                            contador++;
+                            $("#" + contador).addClass("text-primary");
                         });
-                console.log("adios");
-            }else{
-                $("#"+id).text("INCORRECTO")
+                
+            } else {
+                $("#" + id).text("INCORRECTO")
                         .addClass("btn btn-block btn-danger btn-lg")
                         .fadeOut("slow")
                         .fadeIn("slow", function () {
-                            sigue(id);
+                            $("#" + contador).addClass("text-danger");
+                            sigue();
+                            contador++;
+                            $("#" + contador).addClass("text-primary");
                         });
             }
+
         }
-        
-        function sigue(id){
+
+        function sigue() {
+            $("#enunciado").text('');
+            $("#respuestas").text('');
+            if (contador < 10) {
+                crearJSON();
+            } else {
+                $("#enunciado").text('');
+                $("#respuestas").text('');
+                $("#enunciado").append('<h1 class="titulo text-center">¡¡¡Has acabado el nivel!!!</h1>');
+                $("#respuestas").append('<h3 class="titulo text-center">Has acertado: ' + aciertos + ' de 10</h3>');
+                if (aciertos >= 5) {
+                    $("#respuestas").append('<button class="btn btn-block btn-success btn-lg yoda">Elegir nivel</button>');
+                } else {
+                    $("#respuestas").append('<button class="btn btn-block btn-danger btn-lg yoda">Elegir nivel</button>');
+                }
+
+                $(document).ready(function () {
+                    $(".yoda").on("click", function () {
+                        $('#nivel').fadeIn('slow');
+                        $('#preguntas').fadeOut();
+                        reinicio();
+                    });
+                });
+            }
+        }
+
+        function reinicio() {
+            contador = 1;
+            aciertos = 0;
             $("#enunciado").text('');
             $("#respuestas").text('');
             crearJSON();
+            for (var i = 1; i <= 10; i++) {
+                $("#" + i).removeClass("text-danger");
+                $("#" + i).removeClass("text-success");
+                $("#" + i).removeClass("text-primary");
+            }
         }
-        
-        
     </script>
-
 </body>
 </html>
